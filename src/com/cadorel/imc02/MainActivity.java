1f5863d90386ea5c88b2572dc328ee8e74bdfbd1
Maël Cadorel <mael.cadorel@laposte.net>
2015-05-04 13:38:54 +0200

    Some cleanup

--- src/com/cadorel/imc02/MainActivity.java
+++ src/com/cadorel/imc02/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends Activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) 
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // "Inflation" du menu (remplit le menu avec les éventuels items qu'il doit contenir.)
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
